ee4472dfe988e9a60c83de74b84ccd82d5dd4479
georgeben <msdcconnect@gmail.com>
2019-01-23 10:28:43 +0100

    Successfully added notes to the database

--- server/app.js
+++ server/app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const bps = require('body-parser');
 const logger = require('./config/winston.config');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
+//Routes
 const apiRoute = require('./routes/index')
 
-
 const app = express();
+NODE_ENV === 'production'? app.use(morgan('combined')) : app.use(morgan('dev'));
+app.use(cors());
+
+app.use(bps.urlencoded({extended: false}));
+app.use(bps.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world');
--- server/config/winston.config.js
+++ server/config/winston.config.js
@@ -4,10 +4,15 @@ const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
         winston.format.simple(),
-        winston.format.colorize(),
+        winston.format.timestamp(),
     ),
     transports: [
-        new winston.transports.Console()
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.simple(),
+                winston.format.timestamp(),
+            )
+        })
     ]
 })
 
