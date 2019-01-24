19e355fbced7ccc87b38f9f64dd202df24c9e9ca
georgeben <msdcconnect@gmail.com>
2019-01-24 13:23:33 +0100

    Added vue router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,43 +1,12 @@
 <template>
   <div id="app">
-    <Navbar />
-    <Notes v-bind:notes="notes" />
-    <AddNote />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Notes from './components/Notes.vue';
-import Navbar from './components/Navbar.vue';
-import AddNote from './components/AddNote.vue';
-import axios from 'axios';
-
-export default {
-  name: 'app',
-  components: {
-    Notes,
-    Navbar,
-    AddNote,
-  },
-  data(){
-    return {
-      name: "Notely",
-      notes: []
-    }
-  },
-  methods: {
-
-  },
-  created(){
-    axios.get('http://localhost:3000/api/notes')
-    .then(res => {
-      console.log("Data",res.data.data);
-      this.notes = res.data.data;
-    })
-    .catch(err => console.log(err));
-  }
-}
-</script>
 
 <style>
 #app {
