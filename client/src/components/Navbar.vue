59715c7b522a3ef6ad983fa4e4267a79642cee2a
georgeben <msdcconnect@gmail.com>
2019-01-25 13:12:49 +0100

    Deleted search from navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -7,11 +7,6 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn search-btn my-2 my-sm-0" type="submit">Search</button>
-        </form>
-
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
                 <router-link to="/" class="nav-link">Home</router-link>
